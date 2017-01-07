ddbe4e8a2d39f38b9493cdcefa8e334ed929e8a6
Dominik Inführ <dominik.infuehr@gmail.com>
2017-01-07 22:17:55 +0100

    reformatted boots.cpp

--- src/boots.cpp
+++ src/boots.cpp
@@ -15,27 +15,39 @@
 using namespace llvm;
 
 int main(int argc, char *argv[]) {
-    LLVMContext TheContext;
-    IRBuilder<> Builder(TheContext);
-    std::unique_ptr<Module> TheModule;
+  LLVMContext TheContext;
+  IRBuilder<> Builder(TheContext);
+  std::unique_ptr<Module> TheModule;
 
-    TheModule = llvm::make_unique<Module>("my cool jit", TheContext);
+  TheModule = llvm::make_unique<Module>("my cool jit", TheContext);
 
-    IntegerType *I32 = Type::getInt32Ty(TheContext);
-    FunctionType *FT =
-        FunctionType::get(I32, false);
+  IntegerType *I32 = Type::getInt32Ty(TheContext);
+  FunctionType *FT = FunctionType::get(I32, false);
 
-    Function *F =
-        Function::Create(FT, Function::ExternalLinkage, Twine("main"), TheModule.get());
-    BasicBlock *BB = BasicBlock::Create(TheContext, "entry", F);
-    Builder.SetInsertPoint(BB);
+  Function *F = Function::Create(FT, Function::ExternalLinkage, Twine("foo"),
+                                 TheModule.get());
+  BasicBlock *BB = BasicBlock::Create(TheContext, "entry", F);
+  Builder.SetInsertPoint(BB);
 
-    Value *RetVal = ConstantInt::get(I32, 10, true);
-    Builder.CreateRet(RetVal);
+  Value *RetVal = ConstantInt::get(I32, 10, true);
+  Builder.CreateRet(RetVal);
 
-    verifyFunction(*F);
+  verifyFunction(*F);
 
-    TheModule->dump();
+  TheModule->dump();
 
-    return 0;
+//   std::unique_ptr<KaleidoscopeJIT> TheJIT;
+//   TheJIT = llvm::make_unique<KaleidoscopeJIT>();
+
+//   auto H = TheJIT->addModule(std::move(TheModule));
+//   auto ExprSymbol = TheJIT->findSymbol("foo");
+//   assert(ExprSymbol && "symbol foo not found");
+
+//   auto foo = reinterpret_cast<int32_t (*)()>(ExprSymbol.getAddress());
+
+//   auto result = foo();
+
+//   std::cout << "result = " << result << std::endl;
+
+  return 0;
 }
